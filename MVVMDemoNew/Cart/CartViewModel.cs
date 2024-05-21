1093b0cf1b24a94eab9300793a7eb45deee4417e
Xander Carruth <xander.carruth@aecom.com>
2024-05-21 17:41:20 -0400

    Changed trace

--- MVVMDemoNew/Cart/CartViewModel.cs
+++ MVVMDemoNew/Cart/CartViewModel.cs
@@ -49,7 +49,7 @@ namespace MVVMDemoNew.Cart
         {
             if (cartItems == null)
             {
-                Trace.WriteLine("Cart Items is null");
+                Trace.WriteLine("Cart Items");
             }
             CartItems.Add(shoppingItem);
             Trace.WriteLine(cartItems);
